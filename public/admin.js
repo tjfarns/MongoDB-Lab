119d8eea95f81cb5cdaa6d13a51e86fd81f844e6
Tamson Farnsworth <tamsonfarns@gmail.com>
2019-11-20 21:18:03 -0700

    Add files via upload

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,79 @@
 var app = new Vue({
   el: '#admin',
-  data: {},
-  methods: {}
+  data: {
+    title: "",
+    desc: "",
+    file: null,
+    addItem: null,
+    items: [],
+    findTitle: "",
+    findItem: null,
+  },
+  created() {
+    this.getItems();
+  },
+  computed: {
+    suggestions() {
+      return this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+    }
+  },
+  methods: {
+    selectItem(item) {
+      this.findTitle = "";
+      this.findItem = item;
+    },
+    async deleteItem(item) {
+      try {
+        console.log(item._id);
+        let response = axios.delete("/api/items/" + item._id);
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async editItem(item) {
+      try {
+        let response = await axios.put("/api/items/" + item._id, {
+          title: this.findItem.title,
+          desc: this.findItem.desc,
+        });
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          title: this.title,
+          path: r1.data.path,
+          desc: this.desc,
+        });
+        this.addItem = r2.data;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+  }
 });
--- public/script.js
+++ public/script.js
@@ -1,5 +1,20 @@
 var app = new Vue({
   el: '#app',
-  data: {},
-  methods: {}
+  data: {
+    items: [],
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
 });
